e7b0e257f2d194a73c3b8c4afda1d71a06ed8e92
kelvin abche <kelvinabache12@gmail.com>
2024-09-12 17:25:25 -0400

    cambiando las variables globales a locales, y encasulando las funciones

--- array.c
+++ array.c
@@ -1,44 +1,68 @@
 #include <stdio.h>
 
-int items[0],length; // for this variable global
+int add_item(int items[],int length); //declaration 
+
+int list_items(int items[], int length);
+
+int remove_item(int items[]);
 
 
-int add_items(void){
+int main(void){   
    
-   int item;
+   int length; 
+   
+   printf("set a size to the array:");
+   scanf("%d", &length);
 
-   for (int index = 0; index <= length; index++){
-     
-    printf("item to be added to the array:");
-    scanf("%d", &item);
+   int items[length];
 
-     items[index] = item; // here add items the array 
-   }
-
-  return 0;
+   add_item(items,length);
+   list_items(items,length);
 }
 
 
-int remove_items(void){
-   // here is remove item
-  return 0;
-}
+// add new items 
+int add_item(int items[], int length) // definition of function 
 
+{
 
+  int item; 
 
-int main(){ 
-   
-   printf(" index the array:");
-   scanf("%d", &length); // here assigning value 
-   
-   items[length]; // here it same 
-
-   add_items();
-   remove_items();   
+  for (int index = 0; index < length; index++)
     
-    for(int index = 0 ; index <= length; index++){
-      printf("%d\n", items[index]);
-    }
+  {
+     printf("add items inside the array:");
+     scanf("%d", &item);
+    
+     items[index] = item;
 
+  }
 
+  return 0;
 }
+
+// remove end item the array  //! error is function 
+int remove_item(int items[]) 
+
+{
+  
+  int length = sizeof(items) / sizeof(items[0]); //!
+
+  items[length] = length - 1; 
+
+  return 0;
+}
+
+// list the items 
+int list_items(int items[], int length) 
+
+{
+  for (int index  = 0; index < length; index++)
+  {
+     printf( "[%d]:%d\n",index, items[index]); 
+  }
+
+  return 0;
+}
+
+
